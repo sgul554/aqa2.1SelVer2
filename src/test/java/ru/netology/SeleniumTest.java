1d6a4974728d8f7537b723756ac6efd8a226f695
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-11-28 17:27:25 +0300

    test failed new

--- src/test/java/ru/netology/SeleniumTest.java
+++ src/test/java/ru/netology/SeleniumTest.java
@@ -47,7 +47,7 @@ public class SeleniumTest {
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.cssSelector("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        assertEquals("Наш менеджер свяжется с вами в ближайшее время.", text.trim());
 
     }
 
